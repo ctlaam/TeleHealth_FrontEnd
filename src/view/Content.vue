0dd8136da5767e49ed3bd5176df0e3f78019f57e
lamct <lamcaothanh@zamba.vn>
2022-12-21 15:19:42 +0700

    update

--- src/view/Content.vue
+++ src/view/Content.vue
@@ -2,7 +2,8 @@
   <section>
     <TheNavbar></TheNavbar>
     <TheHeader></TheHeader>
-    <TheContent></TheContent>
+    <TheLoading v-if="isLoading"></TheLoading>
+    <TheContent v-else></TheContent>
   </section>
 </template>
 
@@ -11,15 +12,27 @@
 import TheContent from "../components/layout/TheContent.vue";
 import TheNavbar from "../components/layout/TheNavbar.vue";
 import TheHeader from "../components/layout/TheHeader.vue";
+import TheLoading from "../components/layout/TheLoading.vue";
 export default {
+  data() {
+    return {
+      isLoading: true,
+    };
+  },
   name: "App",
   components: {
     TheContent,
     TheNavbar,
     TheHeader,
+    TheLoading,
+  },
+  created() {
+    const me = this;
+    setTimeout(() => {
+      me.isLoading = false;
+    }, 5000);
   },
 };
 </script>
 
-<style>
-</style>
+<style></style>
