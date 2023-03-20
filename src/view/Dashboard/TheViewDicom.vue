49a8dc96d3110a32babee57c312079296b552964
Cao Thanh Lâm <lamct>
2023-03-20 15:10:31 +0700

    update

--- src/view/Dashboard/TheViewDicom.vue
+++ src/view/Dashboard/TheViewDicom.vue
@@ -1,19 +1,39 @@
 <template>
+  <div>
     <iframe
+      v-show="show"
       class="iframe"
       src="http://localhost:3000/"
       title="W3Schools Free Online Web Tutorials"
     ></iframe>
+
+    <div
+      class="red"
+      style="color: red; width: 200px; height: 200px"
+      v-show="!show"
+    ></div>
+  </div>
 </template>
 
 <script>
-export default {};
+export default {
+  data() {
+    return {
+      show: false,
+    };
+  },
+  mounted() {
+    document.querySelector(".iframe").onload = () => {
+      this.show = true;
+    };
+  },
+};
 </script>
 
 <style scoped>
 .iframe {
   margin-top: 20px;
-  margin-left:-5px;
+  margin-left: -5px;
   width: 100%;
   height: 800px;
   border-radius: 10px;
